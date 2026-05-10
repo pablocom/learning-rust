7fa7840757c214a052bf5a0e0dd2f5991f4e0f24
Pablo Company <p.c.ramirez5150@gmail.com>
2026-05-10 22:26:47 +0200

    Add fahrenheit to celsius with unit tests

---
+++ variables/src/farenheit_celsius_converter.rs
@@ -0,0 +1,26 @@
+#[allow(dead_code)]
+pub fn fahrenheit_to_celsius(farenheit: f64) -> f64 {
+    (farenheit - 32.) * 5. / 9.
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_fahrenheit_to_celsius_scenarios() {
+        let cases = [
+            (32., 0.),
+            (212., 100.),
+            (-40., -40.),
+            (98.6, 37.),
+            (-459.67, -273.15),
+        ];
+
+        for (input, expected) in cases {
+            let actual = fahrenheit_to_celsius(input);
+            
+            assert_eq!(actual, expected, "Failed at {}°F: expected {}, got {}", input, expected, actual);
+        }
+    }
+}
--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,21 +1,57 @@
+mod farenheit_celsius_converter;
+
+use std::io;
+
 const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
 
 fn main() {
     println!("three hours are {THREE_HOURS_IN_SECONDS} seconds");
 
     let mut x = 5;
-    println!("x just initialized is {x}");
+    println!("x just initialized is -> {x}");
     
     x = x + 1;
-    println!("x reassigned to {x}");
+    println!("x reassigned to -> {x}");
 
     let x = x + 27;
-    println!("x shadowed with value {x}");    
+    println!("x shadowed with value -> {x}");    
 
     {
         let x = x * 69;
-        println!("x shadowed within a scope {x}");   
+        println!("x shadowed within a scope -> {x}");   
     }
 
-    println!("x value at scope exit {x}");   
+    println!("x value at scope exit -> {x}");   
+
+    let tup = (500, 6.4, 1u8);
+
+    let (x, y, z) = tup;
+    println!("Point coordinates (x, y, z) -> ({x}, {y}, {z})");
+
+    let numbers = [1, 2, 3, 4, 5];
+    let last_element = numbers[4];
+    println!("Last elemenent of an array -> {last_element}");
+
+    testing_arrays();
+}
+
+fn testing_arrays() {
+    let a = [1, 2, 3, 4, 5];
+
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = a[index];
+
+    println!("The value of the element at index {index} is: {element}");
 }
