1fe1d7549afad34ac0f9cd250dc2738c12a8bf8f
pablocom <p.c.ramirez5150@gmail.com>
2026-09-16 12:28:47 +0200

    feat: Add unsage and external projects, remove hello cargo

---
+++ unsafe_superpowers/src/main.rs
@@ -0,0 +1,28 @@
+use std::slice;
+
+fn main() {
+    let mut num = 5;
+
+    let r1 = &raw const num;
+    let r2 = &raw mut num;
+
+    unsafe {
+        println!("r1 is: {}", *r1);
+        println!("r2 is: {}", *r2);
+    }
+}
+
+#[allow(dead_code)]
+fn split_at_mut(values: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
+    let len = values.len();
+    let ptr = values.as_mut_ptr();
+
+    assert!(mid <= len);
+
+    unsafe {
+        (
+            slice::from_raw_parts_mut(ptr, mid),
+            slice::from_raw_parts_mut(ptr.add(mid), len - mid),
+        )
+    }
+}
