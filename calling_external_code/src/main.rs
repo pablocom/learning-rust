0af5a0ea760e1c91894e99b86d964fa7ed5d7bc4
pablocom <p.c.ramirez5150@gmail.com>
2026-09-16 13:02:20 +0200

    feat: Before final project

--- calling_external_code/src/main.rs
+++ calling_external_code/src/main.rs
@@ -1,3 +1,32 @@
+static mut COUNTER: u32 = 0;
+
 fn main() {
-    println!("Hello, world!");
+    let negative_number = -32;
+
+    println!(
+        "Absolute value of {} according to C is {}",
+        negative_number,
+        abs(negative_number)
+    );
+
+    unsafe {
+        // SAFETY: This is only called from a single thread in `main`.
+        for _ in 1..=10000 {
+            add_to_count(3);
+        }
+        println!("COUNTER = {}", COUNTER);
+    }
+}
+
+unsafe extern "C" {
+    safe fn abs(input: i32) -> i32;
+}
+
+/// SAFETY: Calling this from more than a single thread at a time is undefined
+/// behavior, so you *must* guarantee you only call it from a single thread at
+/// a time.
+unsafe fn add_to_count(inc: u32) {
+    unsafe {
+        COUNTER += inc;
+    }
 }
